992ec8ad4846d8dceaf9d22b85d518172f7be166
Jackson Kearl <jakearl@microsoft.com>
2020-10-08 15:53:35 -0700

    Build script blah

--- build-distro.sh
+++ build-distro.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+rm -rf distro
 PACKAGE_ID=$(node -p -e "require('./package.json').name+'_'+require('./package.json').version")
 echo $PACKAGE_ID
 rsync -ma --exclude '*.ts' out src manifest.json package.json LICENSE README.md icons distro
